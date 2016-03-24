2e14b463213b6e3888463ab0c5f6a9d080387466
MGApcDev <mgapcdev@gmail.com>
2016-03-24 13:50:48 +0100

    Update load settings implementation

--- scripts/trelloDate.js
+++ scripts/trelloDate.js
@@ -17,9 +17,17 @@ $(document).ready(function () {
     return intArr.sort();
   }
   
-  var trelloBoard = $('#board');
+  var trelloBoard = $('#board'),
+      currentDate = new Date(),
+      month = currentDate.getMonth() + 1,
+      day = currentDate.getDate(),
+      year = currentDate.getFullYear(),
+      smallDateRegex = /^[\s]*[\d]{1,2}[\s]+[a-zA-Z]{1,15}[\s]*$/;
+
+  // Overwrite to get day to --> 00:00:00
+  currentDate = new Date(month + ' ' + day + ' ' + year);
+  
   function applyDateEnchance(settingsMap, sortedKeys) {
-    
     trelloBoard.find('[class*="is-due-"]').each(function (index, obj) {
       var ele = $(this);
       var trelloDate = ele.find('.badge-text').text();
@@ -32,6 +40,7 @@ $(document).ready(function () {
       var diffDays = Math.ceil(timeDiff / (86400000)); // 1000 * 60 * 60 * 24
       
       sortedKeys.forEach(function (key) {
+        console.log("compare --> " + key + " to " + diffDays);
         if (key <= diffDays) {
           console.log("color --> " + settingsMap[key]);
           ele.css('background-color', '#' + settingsMap[key]);
@@ -46,13 +55,6 @@ $(document).ready(function () {
   
   loadSettings();  
   
-  var currentDate = new Date(),
-      month = currentDate.getMonth() + 1,
-      day = currentDate.getDate(),
-      year = currentDate.getFullYear();
-  // Overwrite to get day to --> 00:00:00
-  currentDate = new Date(month + ' ' + day + ' ' + year);
-  var smallDateRegex = /^[\s]*[\d]{1,2}[\s]+[a-zA-Z]{1,15}[\s]*$/;
 
   // Trello date badge classes
   // is-due-past, is-due-now, is-due-soon, is-due-future
